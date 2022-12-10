653719529525dd97754560fe526d77f5a7294027
hamzaaitbenyissa <hamzaaitbenyissa@gmail.com>
2022-12-10 11:15:34 +0100

    add LoggingAspect

--- springaop/src/main/java/com/benyissa/Main.java
+++ springaop/src/main/java/com/benyissa/Main.java
@@ -5,7 +5,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = {"com.benyissa.services"})
+@ComponentScan(basePackages = {"com.benyissa"})
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
---
+++ springaop/src/main/java/com/benyissa/aspects/LoggingAspect.java
@@ -0,0 +1,26 @@
+package com.benyissa.aspects;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+@Aspect
+@EnableAspectJAutoProxy
+public class LoggingAspect {
+    @Around("execution(* com.benyissa..process(..))")
+    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object result = null;
+        Date t1 = new Date();
+        System.out.println("Logging Aspect ===> Before execution ");
+        result = joinPoint.proceed();
+        Date t2 = new Date();
+        System.out.println("Logging Aspect ===> After execution");
+        System.out.println("Execution Duration : " + (t2.getTime() - t1.getTime()));
+        return result;
+    }
+}
